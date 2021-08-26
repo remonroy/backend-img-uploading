662c5873d6e7aa7af59c901da4a0ba63916c9647
remon roy <remonroy34@gmail.com>
2021-08-27 00:47:33 +0600

    second commit

--- index.js
+++ index.js
@@ -2,88 +2,91 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const multer = require('multer')
-const path =require('path')
-
+const path = require('path')
 
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 app.use(cors())
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
-const UPLOADS_FILE = './uploads/'
+
+const UPLOAD_FILES = './uploads/'
+
 
 const storage = multer.diskStorage({
-    destination:(req,file,cd)=>{
-        cd(null,UPLOADS_FILE)
+    destination:(req,file,cb)=>{
+        cb(null,UPLOAD_FILES)
     },
-    filename:(req,file,cd)=>{
+    filename:(req,file,cb)=>{
         const fileExt = path.extname(file.originalname)
-        const fileName = file.originalname
-                        .replace(fileExt,"")
-                        .toLowerCase()
-                        .split(" ")
-                        .join("-") + "-" + Date.now()
-        cd(null,fileName + fileExt)
+        const fileName =file.originalname
+                            .replace(fileExt,'')
+                            .toLowerCase()
+                            .split(" ")
+                            .join("-")+ "-" + Date.now()
+        cb(null,fileName + fileExt)
     }
+    
 })
-
 
 
 const upload = multer({
     storage:storage,
     limits:{
-        fileSize:1000000, //1MB file
+        fileSize:1000000 //1MB
     },
-    fileFilter:(req,file,cd)=>{
+    fileFilter:(req,file,cb)=>{
         if (file.fieldname === 'avatar') {
             if (
                 file.mimetype === 'image/jpeg' ||
                 file.mimetype === 'image/jpg' ||
                 file.mimetype === 'image/png'
             ) {
-                cd(null,true)
+                cb(null,true)
             }else{
-                cd(new Error('Only uploaded jpeg,png or jpg...!'))
+                cb(new Error('Uploaded file jpeg,jpg or png file supported..?'))
             }
         }else if(file.fieldname === 'doc'){
             if (
                 file.mimetype === 'application/pdf'
             ) {
-                cd(null,true)
+                cb(null,true)
             }else{
-                cd(new Error('Only used pdf file..?'))
+                cb(new Error('Uploaded file pdf supported..?'))
             }
         }
-        
     }
 })
-
 
 app.post("/",upload.fields([
     {name:'avatar',maxCount:1},
     {name:'doc',maxCount:1},
 ]),(req,res)=>{
-    const picName=req.files.avatar[0].filename
+    const avatarName=req.files.avatar[0].filename
     const docName=req.files.doc[0].filename
-    console.log(picName,docName);
+    console.log(avatarName, docName);
+
+
+
     res.json({message:'Hello bangladesh'})
-})
-app.use((err,req,res,next)=>{
-    if (err) {
-        if (err instanceof multer.MulterError) {
-            res.status(500).send('There was a multer error..?')
-        }else{
-            res.status(500).send(err.message)
-        }
-    }else{
-        res.status(200).send('File upload success..!')
-    }
 })
 
 app.get("/",(req,res)=>{
     res.json({message:'Hello everyone'})
 })
 
+app.use((err,req,res,next)=>{
+    if (err) {
+        if (err instanceof multer.MulterError) {
+            res.status(500).send('Multer error defined...?')
+        }else{
+            res.status(500).send(err.message)
+        }
+    }else{
+        res.status(200).send('success')
+    }
+})
 const PORT =process.env.PORT || 4000
 
 app.listen(PORT,()=>{
